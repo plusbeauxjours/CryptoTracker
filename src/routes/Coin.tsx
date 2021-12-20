3d5bf47d992ec3eeb9bedd4eea70d1fdd40f9cae
🫒plusbeauxjours <plusbeauxjours@gmail.com>
2021-12-20 21:27:11 +0700

    set: Home

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,11 +1,14 @@
+import React from "react";
 import { useParams } from "react-router";
 
 interface RouteParams {
   coinId?: string;
 }
 
-function Coin() {
+const Coin: React.FC = () => {
   const { coinId } = useParams() as RouteParams;
+
   return <h1>Coin: {coinId}</h1>;
-}
+};
+
 export default Coin;
